6484371eb4c12319af13b640d06357bef559898f
Vladislav-Pavlenko <151663067+Vladislav-Pavlenko@users.noreply.github.com>
2024-05-15 10:50:48 +0300

    correct

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,9 +4,6 @@ export async function fetchImage(searchValue, page) {
   axios.defaults.baseURL = 'https://pixabay.com/api/';
   const paramsString = `?key=${API_KEY}&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=15`;
 
-  const response = await axios.get(paramsString);
-  if (response.status !== 200) {
-    throw new Error(response.status);
-  }
-  return response.data;
+  const { data } = await axios.get(paramsString);
+  return data;
 }
--- src/main.js
+++ src/main.js
@@ -12,15 +12,20 @@ const refs = {
   loadMoreBtn: document.querySelector('.load-more'),
 };
 
-let userData = '';
+let searchValue = '';
+let page = 1;
+
+let gallery = new SimpleLightbox('#gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 function onFormSubmit(event) {
   event.preventDefault();
   refs.loadMoreBtn.classList.remove('is-visible');
-  let page = 1;
+  page = 1;
   refs.galleryEL.innerHTML = '<span class="loader"></span>';
-  const searchValue = refs.inputEL.value.trim();
-  userData = searchValue;
+  searchValue = refs.inputEL.value.trim();
   if (searchValue === '') {
     return iziToast.show({
       title: '⨻',
@@ -32,7 +37,7 @@ function onFormSubmit(event) {
     });
   }
 
-  fetchImage(searchValue).then(data => {
+  fetchImage(searchValue, page).then(data => {
     if (data.totalHits === 0) {
       return iziToast.show({
         title: '⨻',
@@ -45,53 +50,41 @@ function onFormSubmit(event) {
       });
     }
     refs.galleryEL.innerHTML = '';
-
+    gallery.refresh();
     renderMarkup(data.hits, refs.galleryEL);
     if (data.totalHits > 15) {
       refs.loadMoreBtn.classList.add('is-visible');
     }
-    let gallery = new SimpleLightbox('#gallery a', {
-      captionsData: 'alt',
-      captionDelay: 250,
-    });
-    gallery.refresh();
-
-    refs.loadMoreBtn.addEventListener('click', () => {
-      page += 1;
-      fetchImage(searchValue, page).then(data => {
-        renderMarkup(data.hits, refs.galleryEL);
-        const galleryEl = document.querySelector('.gallery');
-        const galleryHeight = galleryEl.getBoundingClientRect().height;
-        let gallery = new SimpleLightbox('#gallery a', {
-          captionsData: 'alt',
-          captionDelay: 250,
-        });
-        gallery.refresh();
-
-        window.scrollBy({
-          top: galleryHeight * 2,
-          behavior: 'smooth',
-        });
-
-        if (data.totalHits > page * 15) {
-          refs.loadMoreBtn.classList.add('is-visible');
-        } else {
-          refs.loadMoreBtn.classList.remove('is-visible');
-          iziToast.show({
-            title: '⨻',
-            titleSize: '20px',
-            message:
-              "We're sorry, but you've reached the end of search results.",
-            color: 'white',
-            backgroundColor: 'navy-blue',
-            position: 'topRight',
-          });
-        }
-      });
-    });
   });
 
   refs.formEL.reset();
 }
+function onLoadMoreBtnClick(event) {
+  page += 1;
+  fetchImage(searchValue, page).then(data => {
+    renderMarkup(data.hits, refs.galleryEL);
+    const galleryEl = document.querySelector('.gallery');
+    const galleryHeight = galleryEl.getBoundingClientRect().height;
+    gallery.refresh();
 
+    window.scrollBy({
+      top: galleryHeight * 2,
+      behavior: 'smooth',
+    });
+
+    if (Math.ceil(data.totalHits / 15) === page) {
+      refs.loadMoreBtn.classList.remove('is-visible');
+      iziToast.show({
+        title: '⨻',
+        titleSize: '20px',
+        message: "We're sorry, but you've reached the end of search results.",
+        color: 'white',
+        backgroundColor: 'navy-blue',
+        position: 'topRight',
+      });
+    }
+  });
+}
 refs.formEL.addEventListener('submit', onFormSubmit);
+
+refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
